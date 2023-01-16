b34c302db7787507b03518092d2874e4518e20d3
RafaHdzCh <64990211+RafaHdzCh@users.noreply.github.com>
2023-01-16 14:51:53 -0600

    [Update] Attack Animations CanRotate

--- Assets/Scripts/Animators/AnimatorManager.cs
+++ Assets/Scripts/Animators/AnimatorManager.cs
@@ -6,7 +6,7 @@ public class AnimatorManager : MonoBehaviour
 {
     [Header("Components")]
     public Animator anim;
-    [HideInInspector] public bool canRotate;
+    [System.NonSerialized] public bool canRotate;
 
     public void PlayTargetAnimation(string targetAnim, bool isInteracting)
     {
